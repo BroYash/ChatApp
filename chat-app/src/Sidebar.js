8728d244cd68b4e41e84d4f23f24c3533e79a48d
BroYash <yashjeetbajwa8@gmail.com>
2020-09-06 13:34:56 +1200

    Initial setup

---
+++ chat-app/src/Sidebar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Sidebar.css'
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import { Avatar, IconButton } from '@material-ui/core'
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import ChatIcon from "@material-ui/icons/Chat"
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_header">
+                <Avatar/>
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ chat-app/src/firbase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_GvieWlT37G1tbn6_UIrk3dVnfo1JGlc",
+    authDomain: "chat-app-b75dd.firebaseapp.com",
+    databaseURL: "https://chat-app-b75dd.firebaseio.com",
+    projectId: "chat-app-b75dd",
+    storageBucket: "chat-app-b75dd.appspot.com",
+    messagingSenderId: "932637933272",
+    appId: "1:932637933272:web:72e87b45f80e8e456ea1ad"
+  };
